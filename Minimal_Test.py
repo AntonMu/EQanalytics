f31dedda67c3dadd7759d7caae402e097800e90f
AntonMu <muehle@berkeley.edu>
2019-07-08 19:28:59 -0700

    Update Minimal_Test.py

--- Minimal_Test.py
+++ Minimal_Test.py
@@ -1,30 +1,40 @@
 import os
 import subprocess
+import time
 
+# First get the references to all folders
+Data_Folder = os.path.join(os.getcwd(),'Data')
+Model_Folder = os.path.join(Data_Folder,'Model_Weights')
 
-def get_parent_dir(n=1):
-    """ returns the n-th parent dicrectory of the current
-    working directory """
-    current_path = os.getcwd()
-    for k in range(n):
-        current_path = os.path.dirname(current_path)
-    return current_path
+#Next download the pre-trained weights
+download_script = os.path.join(Model_Folder,'Download_Weights.py')
 
+print('Downloading Pretrained Weights')
+start = time.time()
+# call_string = ' '.join(['python',download_script,'1aPCwYXFAOmklmNMLMh81Yduw5UrbHqkN', os.path.join(Model_Folder,'Houses','trained_weights_final.h5') ])
 
-Data_Folder = os.path.join(get_parent_dir(2),'Data')
-Facade_Folder = os.path.join(Data_Folder,'CMP_Facade_DB')
-YOLO_filename = os.path.join(Facade_Folder,'data_all_train.txt')
-log_dir = os.path.join(Data_Folder,'Model_Weights','Openings')
-YOLO_classname = os.path.join(log_dir,'data_all_classes.txt')
+# subprocess.call(call_string , shell=True)
 
-def make_call_string(arglist):
-	result_string = ''
-	for arg in arglist:
-		result_string+= ''.join(['--',arg[0],' ', arg[1],' '])
-	return result_string
+# call_string = ' '.join(['python',download_script, '1FbvHzQWCjucXPbTbI4S1MnBLkAi58Mxv', os.path.join(Model_Folder,'Openings','trained_weights_final.h5') ])
 
-arglist = [['annotation_file',YOLO_filename],['classes_file',YOLO_classname],['log_dir',log_dir]]
-call_string = ' '.join(['python Train.py',make_call_string(arglist)])
+# subprocess.call(call_string , shell=True)
+end = time.time()
+print('Downloaded Pretrained Weights in {0:.1f} seconds'.format(end-start))
 
-print('Calling', call_string)
-subprocess.call(call_string, shell=True)
+# Data_Folder = os.path.join(get_parent_dir(2),'Data',Min)
+# Facade_Folder = os.path.join(Data_Folder,'CMP_Facade_DB')
+# YOLO_filename = os.path.join(Facade_Folder,'data_all_train.txt')
+# log_dir = os.path.join(Data_Folder,'Model_Weights','Openings')
+# YOLO_classname = os.path.join(log_dir,'data_all_classes.txt')
+
+# def make_call_string(arglist):
+# 	result_string = ''
+# 	for arg in arglist:
+# 		result_string+= ''.join(['--',arg[0],' ', arg[1],' '])
+# 	return result_string
+
+# arglist = [['annotation_file',YOLO_filename],['classes_file',YOLO_classname],['log_dir',log_dir]]
+# call_string = ' '.join(['python Train.py',make_call_string(arglist)])
+
+# print('Calling', call_string)
+# subprocess.call(call_string, shell=True)
